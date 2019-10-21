89ed64f1a0c081c3976ec092d3c175fb88a3ddd6
Philipp Schlieker <philipp@schliekers.de>
2019-10-21 10:38:13 +0200

    Init Lab6

---
+++ Lab6/k-means.py
@@ -0,0 +1,55 @@
+# Init the centroids with one example out of each class
+# Compute the average image of each of the clusters
+
+import numpy as np
+import pandas as pd
+from sklearn.cluster import KMeans
+from sklearn.metrics import accuracy_score
+from PIL import Image
+import random
+import matplotlib.pyplot as plt
+
+#Split dataset into training et testing sets
+dir_path = "./dataset/"
+train_set = pd.read_csv(dir_path + "optdigits.tra")
+train_set = np.array(train_set)
+test_set = pd.read_csv(dir_path + "optdigits.tes")
+test_set = np.array(test_set)
+
+#Shufle data to be able to use parts as smaller training set
+np.random.shuffle(train_set)
+np.random.shuffle(test_set)
+
+#Split X & y
+X_train = train_set[:,:64]
+y_train = train_set[:,64]
+X_test = test_set[:,:64]
+y_test = test_set[:,64]
+
+#Init centroids
+centroids = np.zeros(shape=(10,64))
+
+for i in range(10):
+    centroids[i] = random.choice(X_train[y_train == i])
+
+#Perform K-Means
+kmeans = KMeans(n_clusters = 10, init = centroids)
+y_train_pred = kmeans.fit_predict(X_train)
+y_test_pred = kmeans.predict(X_test)
+
+#Output Accuracy
+print("Accuracy score on training set: "+ str(accuracy_score(y_train, y_train_pred) * 100))
+print("Accuracy score on testing set: "+ str(accuracy_score(y_test, y_test_pred) * 100))
+
+#Display prototype of each digit
+for i in range(10):
+    prot = np.mean(X_test[y_test == i], axis = 0)
+    prot = np.reshape(prot, (8,8))
+
+    if i == 0:
+        prototype = prot
+    else:
+        prototype = np.concatenate((prototype, prot), axis=1)
+
+plt.matshow(prototype, cmap=plt.cm.gray)
+plt.show()
