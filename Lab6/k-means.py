51fcc5af2bd1f5c98c8a343962fe1ecd62f61059
Philipp Schlieker <philipp@schliekers.de>
2019-10-21 10:44:57 +0200

    Remove Notes

--- Lab6/k-means.py
+++ Lab6/k-means.py
@@ -1,6 +1,3 @@
-# Init the centroids with one example out of each class
-# Compute the average image of each of the clusters
-
 import numpy as np
 import pandas as pd
 from sklearn.cluster import KMeans
@@ -53,5 +50,5 @@ for i in range(10):
     else:
         prototype = np.concatenate((prototype, prot), axis=1)
 
-plt.matshow(prototype, cmap=plt.cm.gray)
+plt.matshow(prototype, cmap='gray')
 plt.show()
